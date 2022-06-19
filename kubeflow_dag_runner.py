92ca8cbb01014799b1eebecd29a73431a42a274c
Omar Abdullahi <aaobare2@gmail.com>
2022-06-19 11:29:48 +0300

    Tuner_fn

--- kubeflow_dag_runner.py
+++ kubeflow_dag_runner.py
@@ -8,7 +8,7 @@ from tfx.proto import trainer_pb2
 
 def run():
   metadata_config = kubeflow_dag_runner.get_default_kubeflow_metadata_config()
-  tfx_image = os.environ.get('*********', None)
+  tfx_image = os.environ.get('KUBEFLOW_TFX_IMAGE', None)
   runner_config = kubeflow_dag_runner.KubeflowDagRunnerConfig(
     kubeflow_metadata_config=metadata_config,
     tfx_image=tfx_image
@@ -21,7 +21,10 @@ def run():
       pipeline_root=pipeline_config.PIPELINE_ROOT_GCS,
       data_path=utils._data_root,
       preprocessing_fn=utils.PREPROCESSING_FN,
+      tuner_fn=utils.TUNER_FN,
       run_fn=utils.RUN_FN,
-      train_args=trainer_pb2.TrainArgs(),
-      eval_args=trainer_pb2.EvalArgs(),
       serving_model_dir=pipeline_config.SERVING_MODEL_DIR_GCS,
+    ))
+if __name__ == '__main__':
+  logging.set_verbosity(logging.INFO)
+  run()
